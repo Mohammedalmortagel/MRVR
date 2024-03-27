49462971036885da2a20ad55c41aab16169a05a0
AlmortageLL <135501682+AlmortageLL@users.noreply.github.com>
2024-03-28 00:38:44 +0200

    Create keyboard.py

---
+++ AarohiX/plugins/play/keyboard.py
@@ -0,0 +1,28 @@
+import pyrogram
+from pyrogram import Client, filters
+from pyrogram.types import ReplyKeyboardMarkup
+from AarohiX.misc import SUDOERS
+from AarohiX import app
+
+admin_keyboard = ReplyKeyboardMarkup([
+    ['تفعيل التواصل', '/broadcast', 'حالة التواصل'],
+    ['ضع قناة الاشتراك', 'حذف قناة الاشتراك'],
+    ['اذاعة للمطورين', 'اذاعة للأساسيين', 'اذاعة للقنوات'],
+    ['اذاعة للكل', 'توجيه للكل'],
+    ['توجيه للمستخدمين', 'توجيه للجروبات', 'توجيه للقنوات'],
+    ['اخفاء الكيبورد ⚒️']
+], resize_keyboard=True)
+
+# دالة للتعامل مع أمر /admin
+@app.on_message(filters.regex("^/start"))
+async def cpanel(_, message: Message): 
+    await message.reply("لوحة المفاتيح الخاصة بالمطور", reply_markup=admin_keyboard)
+
+# دالة للتعامل مع الأوامر الأخرى
+@app.on_message(filters.text & ~filters.command("اخفاء الكيبورد ⚒️") & filters.private & SUDOERS)
+async def upbkgt(client: Client, message: Message):
+    await message.reply_text(
+        text="""❎ ¦ تم حذف الكيبورد بنجاح""",
+        reply_markup=ReplyKeyboardRemove()
+    )
+    pass
