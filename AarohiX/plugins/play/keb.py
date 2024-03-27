91504bd73d0dc5462d9ae16fb90a2700fd1717d8
AlmortageLL <135501682+AlmortageLL@users.noreply.github.com>
2024-03-27 06:41:11 +0200

    Update keb.py

--- AarohiX/plugins/play/keb.py
+++ AarohiX/plugins/play/keb.py
@@ -62,7 +62,7 @@ REPLY_MESSAGE_BUTTONS = [
     ]
 ]
 
-@app.on_message(filters.regex("^/start"), group=39)
+@app.on_message(filters.regex("^/start"))
 async def cpanel(_, message: Message):             
         text = REPLY_MESSAGE
         reply_markup = ReplyKeyboardMarkup(REPLY_MESSAGE_BUTTONS, resize_keyboard=True, selective=True)
