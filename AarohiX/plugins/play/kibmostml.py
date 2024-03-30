e892cfba7c908e2bb298b52ab27df24f02a5738f
AlmortageLL <135501682+AlmortageLL@users.noreply.github.com>
2024-03-30 13:02:39 +0200

    Update kibmostml.py

--- AarohiX/plugins/play/kibmostml.py
+++ AarohiX/plugins/play/kibmostml.py
@@ -1,20 +1,22 @@
 import asyncio
-from pyrogram import Client, filters
-from random import choice
+
+import os
+import time
+import requests
+from config import START_IMG_URL
 from pyrogram import filters
-from config import BANNED_USERS
-from AarohiX import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
-from typing import Union
-from pyrogram.types import InlineKeyboardButton
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup, ReplyKeyboardRemove, Message, ChatJoinRequest
+import random
+from pyrogram import Client
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup
+
 from AarohiX import app
 from random import  choice, randint
 
-@app.on_message(filters.command(["صوره", "🕷", "صورهه", "صور"], ""))
-async def ihd(client: Client, message: Message):
+@app.on_message(filters.command(["صوره", "صور", "صورهه", "صور"], ""))
+async def almortagel(client: Client, message: Message):
     rl = random.randint(2,75)
     url = f"https://t.me/vnnkli/{rl}"
-    await client.send_photo(message.chat.id,url,caption="💙 ¦ تـم اختيـار صوره لـك",
+    await client.send_photo(message.chat.id,url,caption="◍ تم اختيار لك صوره \n\n √",
     reply_markup=InlineKeyboardMarkup(
             [
                 [
@@ -27,10 +29,10 @@ async def ihd(client: Client, message: Message):
 
 
 @app.on_message(filters.command(["انميي", "انمي"], ""))
-async def ihd(client: Client, message: Message):
+async def almortagel(client: Client, message: Message):
     rl = random.randint(3,153)
     url = f"https://t.me/LoreBots7/{rl}"
-    await client.send_photo(message.chat.id,url,caption="💙 ¦ تـم اختيـار انمي لـك",
+    await client.send_photo(message.chat.id,url,caption="تم اختيار لك انمي بنجاح \n\n √",
     reply_markup=InlineKeyboardMarkup(
             [
                 [
@@ -43,10 +45,10 @@ async def ihd(client: Client, message: Message):
 
 
 @app.on_message(filters.command(["متحركه. 🎬", "متحركه"], ""))
-async def ihd(client: Client, message: Message):
+async def almortagel(client: Client, message: Message):
     rl = random.randint(2,926)
     url = f"https://t.me/GifWaTaN/{rl}"
-    await client.send_animation(message.chat.id,url,caption="💙 ¦ تـم اختيـار ملصق لـك",
+    await client.send_animation(message.chat.id,url,caption="◍ تم اختيار لك ملصق \n\n √",
     reply_markup=InlineKeyboardMarkup(
             [
                 [
@@ -58,10 +60,10 @@ async def ihd(client: Client, message: Message):
     )
 
 @app.on_message(filters.command(["تلاوات", "تلاوة"], ""))
-async def ihd(client: Client, message: Message):
+async def almortagel(client: Client, message: Message):
     rl = random.randint(24,618)
     url = f"https://t.me/EIEI06/{rl}"
-    await client.send_voice(message.chat.id,url,caption="🥹♥ ¦ تـم اختيـار تلاوة قرآنيه لـك",
+    await client.send_voice(message.chat.id,url,caption="◍ تم اختيار لك تلاوه \n\n √",
     reply_markup=InlineKeyboardMarkup(
             [
                 [
@@ -71,12 +73,12 @@ async def ihd(client: Client, message: Message):
             ]
         )
     )
-    
+
 @app.on_message(filters.command(["اقتباسات", "اقتباس"], ""))
-async def ihd(client: Client, message: Message):
+async def almortagel(client: Client, message: Message):
     rl = random.randint(3,102)
     url = f"https://t.me/LoreBots9/{rl}"
-    await client.send_photo(message.chat.id,url,caption="💙 ¦ تـم اختيـار اقتباس لـك",
+    await client.send_photo(message.chat.id,url,caption="◍ تم اختيار لك اقتباس \n\n √",
     reply_markup=InlineKeyboardMarkup(
             [
                 [
@@ -88,10 +90,10 @@ async def ihd(client: Client, message: Message):
     )
 
 @app.on_message(filters.command(["هيدرا", "هيدرات"], ""))
-async def ihd(client: Client, message: Message):
+async def almortagel(client: Client, message: Message):
     rl = random.randint(2,153)
     url = f"https://t.me/flflfldld/{rl}"
-    await client.send_photo(message.chat.id,url,caption="💙 ¦ تـم اختيـار هيدرات لـك",
+    await client.send_photo(message.chat.id,url,caption="◍ تم اختيار لك هيدرا بنجاح \n\n √",
     reply_markup=InlineKeyboardMarkup(
             [
                 [
@@ -103,10 +105,10 @@ async def ihd(client: Client, message: Message):
     )
 
 @app.on_message(filters.command(["صور", "افاتار بنات"], ""))
-async def ihd(client: Client, message: Message):
+async def almortagel(client: Client, message: Message):
     rl = random.randint(2,216)
-    url = f"https://t.me/vvyuol/{rl}"
-    await client.send_photo(message.chat.id,url,caption="💙 ¦ تـم اختيـار افاتار بنات لـك",
+    url = f"https://t.me/photo_teto1/{rl}"
+    await client.send_photo(message.chat.id,url,caption="◍ تم اختيار لك افتار بنات \n\n √",
     reply_markup=InlineKeyboardMarkup(
             [
                 [
@@ -118,10 +120,10 @@ async def ihd(client: Client, message: Message):
     )
 
 @app.on_message(filters.command(["صور شباب", "افاتار شباب"], ""))
-async def ihd(client: Client, message: Message):
+async def almortagel(client: Client, message: Message):
     rl = random.randint(2,148)
     url = f"https://t.me/vgbmm/{rl}"
-    await client.send_photo(message.chat.id,url,caption="💙 ¦ تـم اختيـار افاتار شباب لـك",
+    await client.send_photo(message.chat.id,url,caption="◍ تم اختيار لك افتار شباب \n\n √",
     reply_markup=InlineKeyboardMarkup(
             [
                 [
@@ -133,10 +135,10 @@ async def ihd(client: Client, message: Message):
     )
 
 @app.on_message(filters.command(["سورة"], ""))
-async def ihd(client: Client, message: Message):
+async def almortagel(client: Client, message: Message):
     rl = random.randint(2,82)
     url = f"https://t.me/opuml/{rl}"
-    await client.send_voice(message.chat.id,url,caption="🥹♥ ¦ تـم اختيـار ايـه قرآنيه لـك",
+    await client.send_voice(message.chat.id,url,caption="◍ تم اختيار لك سوره قرآنيه \n\n √",
     reply_markup=InlineKeyboardMarkup(
             [
                 [
@@ -148,40 +150,10 @@ async def ihd(client: Client, message: Message):
     )
 
 @app.on_message(filters.command(["الشيخ", "النقشبندي", "نقشبندي"], ""))
-async def ihd(client: Client, message: Message):
+async def almortagel(client: Client, message: Message):
     rl = random.randint(2,114)
     url = f"https://t.me/ggcnjj/{rl}"
-    await client.send_voice(message.chat.id,url,caption="🥹♥ ¦ تـم اختيـار الشيخ نقشبندي لـك",
-    reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        message.from_user.first_name, url=f"https://t.me/{message.from_user.username}")
-                ],
-            ]
-        )
-    )
-    
-@app.on_message(filters.command(["عبدالباسط", "عبدالباسط عبدالصمد"], ""))
-async def ihd(client: Client, message: Message):
-    rl = random.randint(7,265)
-    url = f"https://t.me/telawatnader/{rl}"
-    await client.send_voice(message.chat.id,url,caption="🥹♥ ¦ تـم اختيـار الشيخ عبدالباسط لـك",
-    reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        message.from_user.first_name, url=f"https://t.me/{message.from_user.username}")
-                ],
-            ]
-        )
-    )
-    
-@app.on_message(filters.command(["استوري", "استوريهات. 🥹"], ""))
-async def ihd(client: Client, message: Message):
-    rl = random.randint(2,148)
-    url = f"https://t.me/yoipopl/{rl}"
-    await client.send_audio(message.chat.id,url,caption="💚 ¦ تـم اختيـار استوري لـك",
+    await client.send_voice(message.chat.id,url,caption="◍ تم اختيار لك الشيخ النقشبندي \n\n √",
     reply_markup=InlineKeyboardMarkup(
             [
                 [
@@ -192,3 +164,78 @@ async def ihd(client: Client, message: Message):
         )
     )
 
+@app.on_message(filters.command(["عبدالباسط", "عبدالباسط عبدالصمد"], ""))
+async def almortagel(client: Client, message: Message):
+    rl = random.randint(7,265)
+    url = f"https://t.me/telawatnader/{rl}"
+    await client.send_voice(message.chat.id,url,caption="◍ تم اختيار لك الشيخ عبد الباسط \n\n √",
+    reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        message.from_user.first_name, url=f"https://t.me/{message.from_user.username}")
+                ],
+            ]
+        )
+    )
+
+@app.on_message(filters.command(["استوري", "استوريهات"], ""))
+async def almortagel(client: Client, message: Message):
+    rl = random.randint(2,148)
+    url = f"https://t.me/yoipopl/{rl}"
+    await client.send_audio(message.chat.id,url,caption="◍ تم اختيار لك استوري \n\n √",
+    reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        message.from_user.first_name, url=f"https://t.me/{message.from_user.username}")
+                ],
+            ]
+        )
+    )
+    
+    
+    @app.on_message(filters.command(["كتب دينية"], ""))
+async def almortagel(client: Client, message: Message):
+    rl = random.randint(2,1959)
+    url = f"https://t.me/kotobeslameah/{rl}"
+    await client.send_document(message.chat.id,url,caption="تـم اختيـار كتاب ديني لـك",
+    reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        message.from_user.first_name, url=f"https://t.me/{message.from_user.username}")
+                ],
+            ]
+        )
+    )
+
+@app.on_message(filters.command(["احاديث دينية"], ""))
+async def almortagel(client: Client, message: Message):
+    rl = random.randint(799,1341)
+    url = f"https://t.me/dmatrix12/{rl}"
+    await client.send_photo(message.chat.id,url,caption="تـم اختيـار احاديث لـك",
+    reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        message.from_user.first_name, url=f"https://t.me/{message.from_user.username}")
+                ],
+            ]
+        )
+    )
+
+@app.on_message(filters.command(["خطب دينيه"], ""))
+async def almortagel(client: Client, message: Message):
+    rl = random.randint(2,201)
+    url = f"https://t.me/fresdewi/{rl}"
+    await client.send_voice(message.chat.id,url,caption="تـم اختيـار خطبه لـك",
+    reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        message.from_user.first_name, url=f"https://t.me/{message.from_user.username}")
+                ],
+            ]
+        )
+    )
