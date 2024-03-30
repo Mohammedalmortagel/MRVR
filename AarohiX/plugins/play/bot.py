e009be60e5fb4460865ea05492430530e25a0d63
AlmortageLL <135501682+AlmortageLL@users.noreply.github.com>
2024-03-30 23:16:09 +0200

    Update bot.py

--- AarohiX/plugins/play/bot.py
+++ AarohiX/plugins/play/bot.py
@@ -2,4 +2,9 @@ import asyncio
 import random
 from AarohiX import app
 from pyrogram.types import (InlineKeyboardButton,                            InlineKeyboardMarkup, Message)
-from pyrogram import filters, Clienttxt = ["يا أجمل من نطق اسمي على لسانه ♡","شتبي","وش بغيت","وش رايك فاللي يناديني بوت ؟ 🦦","اسمى سهيله ياحب 🙄❤️","ثانيه واحده بتشقط وجى🙄","اؤمر {1} شتريد؟❤️🥺",        ]        @app.on_message(filters.command(["بوت","المرتجل"]))async def ktbat(client: Client, message: Message):      a = random.choice(txt)      await message.reply(f"https://t.me/{app.username}?startgroup=true")
+from pyrogram import filters, Client
+txt = ["يا أجمل من نطق اسمي على لسانه ♡","شتبي","وش بغيت","وش رايك فاللي يناديني بوت ؟ 🦦","اسمي\n{1}\nيحب","ثانيه واحده بتشقط وجى🙄",        ]   
+@app.on_message(filters.command(["بوت","المرتجل"]))
+async def ktbat(client: Client, message: Message):  
+url= f"https://t.me/{app.username}?startgroup=true"
+a = random.choice(txt,url)      await message.reply(f"{a}")
