1dc12f59c7c4fd28234ef2ab17528390a168b81f
AlmortageLL <135501682+AlmortageLL@users.noreply.github.com>
2024-03-31 19:45:00 +0200

    Update ترحيب.py

---
+++ AarohiX/plugins/play/ترحيب.py
@@ -0,0 +1,27 @@
+import asyncio
+import random
+from AarohiX.misc import SUDOERS
+from pyrogram.types import (Message,InlineKeyboardButton,InlineKeyboardMarkup,CallbackQuery,ChatPrivileges)
+from pyrogram import filters, Client
+from AarohiX import app
+from config import *
+
+
+almortagel_responses = [
+    "- اقوي بوت ميوزك قنوات و جروبات سرعه وجوده خارقه\n\nوبدون تهنيج او تقطيع او توقف وكمان ان البوت في مميزات جامدة⚡️♥️.\n\nارفع البوت ادمن فقناتك او جروبك واستمتع بجوده الصوت و السرعه الخياليه للبوت ⚡️♥️\n\nمعرف البوت 🎸 [ {app.username} ]\n\n➤ 𝘉𝘰𝘵 𝘵𝘰 𝘱𝘭𝘢𝘺 𝘴𝘰𝘯𝘨𝘴 𝘪𝘯 𝘷𝘰𝘪𝘤e 𝘤𝘩𝘢𝘵 ♩🎸 \n\n-𝙱𝙾𝚃 ➤{app.username}\n\n-𝙳𝙴𝚅 ➤ user_id={config.OWNER_ID}"
+       
+]
+
+@app.on_message(filters.command(["ترند"], ""), group=39)
+async def almortagel_bot(client, message):
+    global name
+    bot_username = (await app.get_me()).username
+    bar = random.choice(almortagel_responses).format(name=name)
+    keyboard = InlineKeyboardMarkup([
+        [InlineKeyboardButton("خدني لجروبك والنبي🥺♥", url=f"https://t.me/{bot_username}?startgroup=True")]
+    ])
+
+    await message.reply(
+       disable_web_page_preview=True,
+        reply_markup=keyboard
+    )
