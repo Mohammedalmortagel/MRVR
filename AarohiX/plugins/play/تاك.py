5bcb4ca134d5ba15a846d932da39fee11b155650
AlmortageLL <135501682+AlmortageLL@users.noreply.github.com>
2024-04-03 00:42:33 +0200

    Update تاك.py

--- AarohiX/plugins/play/تاك.py
+++ AarohiX/plugins/play/تاك.py
@@ -3,7 +3,7 @@ import os
 import time
 import requests
 import aiohttp
-from pyrogram import filters.command
+from strings.filters import command
 from pyrogram import filters
 from pyrogram import Client
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup
