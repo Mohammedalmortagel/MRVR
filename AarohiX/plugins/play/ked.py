9d987ed3fdad02996837df2bbc95bac2fd315e82
AlmortageLL <135501682+AlmortageLL@users.noreply.github.com>
2024-04-05 04:26:57 +0200

    Update ked.py

--- AarohiX/plugins/play/ked.py
+++ AarohiX/plugins/play/ked.py
@@ -1,21 +1,17 @@
-import pyrogram
-from pyrogram import Client, filters
-from config import BANNED_USERS, OWNER_ID 
-from pyrogram.types import ReplyKeyboardMarkup, ReplyKeyboardRemove
-from AarohiX.misc import SUDOERS
-from AarohiX import app
-
-@app.on_message(filters.regex("^/admin") & filters.user(OWNER_ID), group=39)
-async def admin(client, message):
-  keyboard = ReplyKeyboardMarkup([
+@Client.on_message(filters.command(["/start","رجوع للقائمة الرئيسيه"], ""))
+async def start(client, message):
+ if not message.chat.type == enums.ChatType.PRIVATE:
+    if await joinch(message):
+            return
+ bot_username = client.me.username
+ dev = await get_dev(bot_username)
+ nn = await get_dev_name(client, bot_username)
+ if message.chat.id == dev or message.chat.username in OWNER:
+   kep = ReplyKeyboardMarkup([
 ["تفعيل التواصل", "/broadcast", "حالة التواصل"],
 ["ضع قناة الاشتراك", "حذف قناة الاشتراك"],
 ["اذاعة للمطورين", "اذاعة للأساسيين", "اذاعة للقنوات"],
 ["اذاعة للكل", "توجيه للكل"],
 ["توجيه للمستخدمين", "توجيه للجروبات", "توجيه للقنوات"],
-["اخفاء الكيبورد ⚒️"]],resize_keyboard=True)
-  await message.reply("لوحة المفاتيح الخاصة بالمطور", reply_markup=keyboard,quote=True)
-
-@app.on_message(filters.command("اخفاء الكيبورد ⚒️") & filters.private & SUDOERS)
-async def admin(client, message):
-  await message.reply("❎ ¦ تم حذف الكيبورد بنجاح",quote=True,reply_markup=ReplyKeyboardRemove(selective=True))
+["اخفاء الكيبورد ⚒️"]], resize_keyboard=True)
+return await message.reply_text("**♪ اهلا بك ، عزيزي المطور الاساسي  💎 .**", reply_markup=kep,quote=True)
