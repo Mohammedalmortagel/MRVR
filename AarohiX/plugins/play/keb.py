7986b8409845e74acacddbd17090aa6ed627bc51
AlmortageLL <135501682+AlmortageLL@users.noreply.github.com>
2024-04-12 03:08:26 +0200

    Update keb.py

--- AarohiX/plugins/play/keb.py
+++ AarohiX/plugins/play/keb.py
@@ -10,7 +10,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, 
                         
 @app.on_message(filters.regex("^/start"), group=39)
 async def cpanel(_, message: Message):
-    if message.from_user.id in SUDOERS:
+    if message.from_user.id in owner_id:
        await message.reply_text(
                 "اهلا عزيزي المطور\nاليك لوحة التحكم الخاصة بالبوت",
                 reply_markup=ReplyKeyboardMarkup(
