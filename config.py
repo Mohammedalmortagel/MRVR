78919979ce627db5a0b02f89ebc6f51f952d466b
AlmortageLL <135501682+AlmortageLL@users.noreply.github.com>
2024-04-24 17:50:52 +0200

    Update config.py

--- config.py
+++ config.py
@@ -43,7 +43,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/AlmortageLL/Alm",
+    "https://github.com/AlmortageLL/Alooo",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
