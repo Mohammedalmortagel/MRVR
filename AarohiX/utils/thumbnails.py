f884e6c27abbce2bbbd18d26374812322f64396c
AlmortageLL <135501682+AlmortageLL@users.noreply.github.com>
2024-04-24 19:32:36 +0200

    Update thumbnails.py

--- AarohiX/utils/thumbnails.py
+++ AarohiX/utils/thumbnails.py
@@ -122,8 +122,8 @@ async def get_thumb(videoid):
             font4 = ImageFont.truetype('AarohiX/assets/font2.ttf', 35)
 
             image4 = ImageDraw.Draw(image2)
-            image4.text((10, 10), "livequran", fill="white", font = font1, align ="left") 
-            image4.text((670, 150), "HMS PLAYING", fill="white", font = font2, stroke_width=2, stroke_fill="white", align ="left") 
+            image4.text((10, 10), "live quran", fill="white", font = font1, align ="left") 
+            image4.text((670, 150), "Hms PLAYING", fill="white", font = font2, stroke_width=2, stroke_fill="white", align ="left") 
 
             # title
             title1 = truncate(title)
