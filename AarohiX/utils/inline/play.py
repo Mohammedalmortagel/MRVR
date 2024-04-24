e670409d010697f79eda5af8e3bda462a5eff712
AlmortageLL <135501682+AlmortageLL@users.noreply.github.com>
2024-04-24 15:52:31 +0200

    Update play.py

--- AarohiX/utils/inline/play.py
+++ AarohiX/utils/inline/play.py
@@ -58,7 +58,7 @@ def stream_markup_timer(_, chat_id, played, dur):
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
         ],
         [
-            InlineKeyboardButton(text=f"{OWNER_NAME}", url="https://t.me/{OWNER[0]}"
+            InlineKeyboardButton(text=f"{OWNER_NAME}", user_id=config.OWNER_ID
             )
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
@@ -82,7 +82,7 @@ def stream_markup(_, chat_id):
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
         ],
         [
-            InlineKeyboardButton(text=f"{OWNER_NAME}", url="https://t.me/{OWNER[0]}"
+            InlineKeyboardButton(text=f"{OWNER_NAME}", user_id=config.OWNER_ID
             )
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
