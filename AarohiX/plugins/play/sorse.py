13e990df444807757b70ad2d65c9dc48096779fc
MOHAMEDELMORT <135501682+MOHAMEDELMORT@users.noreply.github.com>
2024-05-12 22:32:37 +0300

    Update sorse.py

--- AarohiX/plugins/play/sorse.py
+++ AarohiX/plugins/play/sorse.py
@@ -42,17 +42,4 @@ async def huhh(client: Client, message: Message):
     )
 
 
-@app.on_message(filters.command(["غنيلي", "غني", "غ", "🎙 ¦ غـنيـلي"], ""))
-async def ihd(client: Client, message: Message):
-    rl = random.randint(3,267)
-    url = f"https://t.me/bsmaatt/{rl}"
-    await client.send_voice(message.chat.id,url,caption="🔥 ¦ تـم اختيـار الاغـنـية لـك",
-    reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        message.from_user.first_name, url=f"https://t.me/{message.from_user.username}")
-                ],
-            ]
-        )
-                           )
+
