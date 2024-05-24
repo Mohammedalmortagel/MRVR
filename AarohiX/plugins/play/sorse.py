2babdbf66f299548a0172567700374597db66f72
Almortagel <115498589+Mohammedalmortagel@users.noreply.github.com>
2024-05-24 21:14:57 +0300

    Update sorse.py

--- AarohiX/plugins/play/sorse.py
+++ AarohiX/plugins/play/sorse.py
@@ -13,24 +13,24 @@ from AarohiX import app
 from random import  choice, randint
 
 @app.on_message(
-    filters.command(["سورس مين","سورس","السورس","سورسي", "HMS"], ""))
+    filters.command(["سورس مين","سورس","السورس","سورسي", "𝗠𝗥 𝗩𝗥"], ""))
 async def huhh(client: Client, message: Message):
     await message.reply_photo(
-        photo=f"https://t.me/botatiiii",
-        caption=f"""Welcome to ѕᴏụʀᴄᴇ ʜᴍѕ""",
+        photo=f"https://telegra.ph/file/e1afd8ed6c94fe4b3a270.jpg",
+        caption=f"""Welcome to ѕᴏụʀᴄᴇ ᴀʟᴍᴏʀᴛᴀɢᴇʟ""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                text="𝚂𝙾𝚄𝚁𝙲𝙴⁩", url=f"https://t.me/botatiiii"
+                text="𝗦𝗼𝘂𝗿𝗰𝗲 𝗩𝗥", url=f"https://t.me/Jaithon"
                         ),
            InlineKeyboardButton(
-                text="𝙶𝚁𝙾𝚄𝙿", url=f"https://t.me/brmjeatt"
+                text="𝗚𝗿𝗼𝘂𝗽", url=f"https://t.me/Source_MRVR"
             ),
         ],
         [
             InlineKeyboardButton(
-                text="˛َِ ٰ𝖍𝑚ƨ 𓃠ㅤㅤㅤㅤㅤㅤ", url=f"https://t.me/hms_01"
+                text="𝗠𝗥 𝗩𝗥", url=f"https://t.me/D_Z_J_C"
             ),
   
                 ],
@@ -42,4 +42,17 @@ async def huhh(client: Client, message: Message):
     )
 
 
-
+@app.on_message(filters.command(["غنيلي", "غني", "غ", "🎙 ¦ غـنيـلي"], ""))
+async def ihd(client: Client, message: Message):
+    rl = random.randint(3,267)
+    url = f"https://t.me/bsmaatt/{rl}"
+    await client.send_voice(message.chat.id,url,caption="🔥 ¦ تـم اختيـار الاغـنـية لـك",
+    reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        message.from_user.first_name, url=f"https://t.me/{message.from_user.username}")
+                ],
+            ]
+        )
+                           )
